40c3d8901730be865d87258ffabca06918e49bdc
Jesus Arriola <piracarter@hotmail.com>
2018-07-05 10:12:18 -0500

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,2 +1,12 @@
-import os
-os.system('echo "$(date) Un simple texto para validar que funciona correctamente un post-commit" | mail -s "Correo enviado desde hook post-commit by-python" root@lab.example.com')
+import smtplib
+
+from email.mime.text import MIMEText
+
+msg = MIMEText("Un simple texto para validar que funciona correctamente un post-commit")
+msg['Subject'] = 'Correo enviado desde hook post-commit by-python'
+msg['From'] = 'root-master@lab.example.com'
+msg['To'] = 'root@lab.example.com'
+
+s = smtplib.SMTP('mail.example.com')
+s.sendmail('root-master@lab.example.com', ['root@lab.example.com'], msg.as_string())
+s.quit()
